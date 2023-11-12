f4a452f1a74bb0500a7017fef42372703b9412cd
Andrey Bakin <andybulkin@yandex.ru>
2023-11-13 00:08:03 +0300

    Создание файла index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+let a = 20;
+alert (a)
+
+let year;
+alert (year = 'Год выпуска первого iPhone - 2007')
+
+let creator;
+alert (creator = 'Создатель языка JavaScript - Брендан Эйх')
+
+let x = 10;
+let y = 2;
+alert (x + y);
+alert (x - y);
+alert (x * y);
+alert (x / y);
+
+let s = 2;
+alert (redult = s ** 5);
