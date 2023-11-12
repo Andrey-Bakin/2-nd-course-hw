52e6c2944b0081188db85ae2b452183aeff7f42b
Andrey Bakin <andybulkin@yandex.ru>
2023-11-13 01:56:07 +0300

    Доработан файл insex.js

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ alert (x / y);
 
 let s = 2;
 alert (redult = s ** 5);
+
+let a1 = 9;
+let b = 2;
+alert (a1 % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert (age);
+
+let user = {
+    name: 'Андрей',
+    age: 20,
+    isAdmin: true,
+}
+user["city of residence"] = 'Рязань';
+user.age = 35;
+delete user["city of residence"];
+let info = prompt("Какую информацию вы хотите узнать о пользователе?");
+alert(user[info]);
