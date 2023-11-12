f1ddf9ee71bac31c124070201a64e687c566ec96
Andrey Bakin <andybulkin@yandex.ru>
2023-11-13 02:10:02 +0300

    Доработан index.js , пронумерованы задания

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// 1
 let a = 20;
 alert (a)
 
+// 2
 let year;
 alert (year = 'Год выпуска первого iPhone - 2007')
 
+// 3
 let creator;
 alert (creator = 'Создатель языка JavaScript - Брендан Эйх')
 
+// 4
 let x = 10;
 let y = 2;
 alert (x + y);
@@ -14,13 +18,16 @@ alert (x - y);
 alert (x * y);
 alert (x / y);
 
+// 5
 let s = 2;
 alert (redult = s ** 5);
 
+// 6
 let a1 = 9;
 let b = 2;
 alert (a1 % b);
 
+// 7
 let num = 1;
 num += 5;
 num -= 3;
@@ -30,9 +37,11 @@ num ++;
 num --;
 alert(num);
 
+// 8
 let age = prompt('Сколько вам лет?');
 alert (age);
 
+// 9
 let user = {
     name: 'Андрей',
     age: 20,
@@ -43,3 +52,7 @@ user.age = 35;
 delete user["city of residence"];
 let info = prompt("Какую информацию вы хотите узнать о пользователе?");
 alert(user[info]);
+
+// 10
+let name = prompt('Как Вас зовут?')
+alert (`Привет, ${name}!`)
