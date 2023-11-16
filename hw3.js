37014e585e3164a72cc0d68cc3cc50b3cb423032
Andrey Bakin <andybulkin@yandex.ru>
2023-11-17 00:41:58 +0300

    Выполнено задание 1 в hw3

---
+++ hw3.js
@@ -0,0 +1,9 @@
+// 1
+let password = 'пароль';
+let askPassword = prompt('Введите пароль');
+
+if (askPassword == 'пароль') {
+	console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+};
