25290b852a1eff698bf56abd5c40fb8cd4fdc1ed
Andrey Bakin <andybulkin@yandex.ru>
2023-11-21 21:58:29 +0300

    Выполнено задание 4

--- hw4.js
+++ hw4.js
@@ -13,3 +13,14 @@ for (let a = 7; a <= 22; a++) {
     console.log (a);
 }
 
+// 4
+let obj = {
+    "Коля": 200,
+    "Вася": 300,
+    "Петя": 400
+}
+
+for (let key in obj) {
+    console.log(`${key} - зарплпта ${obj[key]} долларов.`)
+}
+
