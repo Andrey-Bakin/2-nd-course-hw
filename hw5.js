8ec081970aed33dba9507743e7919eba653053a7
Andrey Bakin <andybulkin@yandex.ru>
2023-11-24 01:39:08 +0300

    Выполнено задание 2

--- hw5.js
+++ hw5.js
@@ -10,3 +10,15 @@ function num(a, b) {
 console.log(num(8, 4))
 console.log(num(6, 6))
 
+// 2
+function number(n) {
+  if (n % 2 === 0) {
+    return 'Число четное'
+  } else {
+    return 'Число нечетное'
+  }
+}
+
+console.log(number(8))
+console.log(number(3))
+
