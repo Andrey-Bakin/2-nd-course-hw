91fcab0efb1f8b4c0cdf198f1d266f8c0cc5a9f1
Andrey Bakin <andybulkin@yandex.ru>
2023-11-28 22:03:06 +0300

    Выполнено задание 2

--- hw6.js
+++ hw6.js
@@ -6,3 +6,9 @@ for (let i = 0; i < numbs.length; i++) {
     console.log(numbs[i]);
 }
 
+// 2
+const numbs = [1, 5, 4, 10, 0, 3];
+
+numbs.forEach((el, index) => {
+	console.log(`${index}: ${el}`);
+});
