86f88b4754133b67b24197d80a7b722be0454b01
Andrey Bakin <andybulkin@yandex.ru>
2023-12-14 22:27:24 +0300

    Создан отдельный js файл игр для блоков game-box1, game-box2, game-box3

--- hw7.js
+++ hw7.js
@@ -1,20 +1,3 @@
-// Задание 8 - hw5 (Игра № 1)
-function getSeason(month) {
-  if (month == 12 || month == 1 || month == 2) {
-    return "Зима";
-  } else if (month >= 3 && month <= 5) {
-    return "Весна";
-  } else if (month >= 6 && month <= 8) {
-    return "Лето";
-  } else if (month >= 9 && month <= 11) {
-    return "Осень";
-  } else {
-    return "Введен неверный параметр!";
-  }
-}
-
-alert(getSeason(prompt("Введите номер месяца")));
-
 // Задание № 1
 let str = "js";
 console.log(str.toUpperCase());
